9dea7828bbe4309b239063044d0d07462089e020
GeorgeLu2018 <37014021+GeorgeLu2018@users.noreply.github.com>
2018-03-03 17:30:22 +0800

    Create mySimpleScroll.js

---
+++ mySimpleScroll.js
@@ -0,0 +1,28 @@
+(function(document){
+   'use strict';
+    
+  var scroll = function(options) {
+    var targetDiv = document.getElementById(options.selector),
+    onlyChild =  targetDiv.firstElementChild,   
+    onlyChildWidth = onlyChild.scrollWidth,
+    scrollContainer = document.createElement('div'),
+    targetDivWidth = targetDiv.style.width,
+    virtualContentDiv = document.createElement('div');
+    
+    scrollContainer.setAttribute('style', 'width:'+targetDivWidth+';height:15px;background:transparent;overflow:auto;'); 
+    virtualContentDiv.setAttribute('style','width:'+ onlyChildWidth +'px;height:15px;background:transparent;');
+    scrollContainer.appendChild(virtualContentDiv);
+    
+    if(targetDiv.nextHtmlSibling) {
+       targetDiv.parentNode.insertBefore(targetDiv.nextHtmlSibling, targetDiv.nextSibling);
+    }else {
+       targetDiv.parentNode.appendChild(scrollContainer);      
+    }
+    
+    scrollContainer.addEventListener('scroll', function() {
+       targetDiv.scrollLeft = this.scrollLeft;
+    });
+  }
+  
+  window.simpleScroll = scroll;
+})(document);
